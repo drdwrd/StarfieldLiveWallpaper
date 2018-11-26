5d5305e8c0ec40b954c0cd75023e86b109abdecf
drdwrd <drwdrd@gmail.com>
2018-11-26 22:14:59 +0100

    still going...

--- Starfield/src/main/assets/shaders/starfield.frag
+++ Starfield/src/main/assets/shaders/starfield.frag
@@ -3,11 +3,34 @@ precision highp float;
 
 
 uniform sampler2D u_Layer0;
+uniform sampler2D u_Layer1;
+uniform sampler2D u_Layer2;
+
+uniform float u_Time;
 
 varying vec2 uv;
 
 
 void main() {
 
-    gl_FragColor = texture2D(u_Layer0, uv);
+
+    vec2 duv0 = vec2(sin(0.05 * u_Time), cos(0.05 * u_Time));
+    vec2 duv1 = vec2(sin(0.1 * u_Time), cos(0.1 * u_Time));
+    vec2 duv2 = vec2(sin(0.15 * u_Time), cos(0.15 * u_Time));
+
+
+    vec4 layer0 = texture2D(u_Layer0, uv + duv0);
+    vec4 layer1 = texture2D(u_Layer1, uv + duv1);
+    vec4 layer2 = texture2D(u_Layer2, uv + duv2);
+
+    float l1 = exp(5.0 * (0.299 * layer1.r + 0.587 * layer1.g + 0.114 * layer1.b));
+    float l2 = exp(5.0 * (0.299 * layer2.r + 0.587 * layer2.g + 0.114 * layer2.b));
+
+    vec4 color = layer2;
+
+    color = 1.0 - (1.0 - l2 * layer1) * (1.0 - layer2);
+
+    color = 1.0 - (1.0 - l1 * layer0) * (1.0 - color);
+
+    gl_FragColor = color;
 }
--- Starfield/src/main/java/drwdrd/ktdev/starfield/StarfieldRenderer.kt
+++ Starfield/src/main/java/drwdrd/ktdev/starfield/StarfieldRenderer.kt
@@ -6,6 +6,7 @@ import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.opengl.GLES20
 import android.opengl.GLSurfaceView
+import android.os.SystemClock
 import android.view.GestureDetector
 import android.view.MotionEvent
 import drwdrd.ktdev.engine.*
@@ -57,9 +58,10 @@ class StarfieldRenderer(_context: Context) : GLSurfaceView.Renderer {
 
     private val context : Context = _context
     private val simplePlane = SimplePlane()
-    private var aspect : Float = 1.0f
-    private lateinit var simplicityShader : ProgramObject
-    private lateinit var layer0 : Texture
+    private var aspect = vector2f(1.0f, 1.0f)
+    private lateinit var shader : ProgramObject
+    private var layers = Array(3) { Texture() }
+    private var startTime : Long = 0
 
 
     fun createGestureListener() = StarfieldGestureListener()
@@ -75,28 +77,46 @@ class StarfieldRenderer(_context: Context) : GLSurfaceView.Renderer {
         Log.info("OpenGL renderer: $renderer")
 
         simplePlane.create()
-        simplicityShader = ProgramObject.loadFromAssets(context, "shaders/starfield.vert", "shaders/starfield.frag", simplePlane.vertexFormat)
+        shader = ProgramObject.loadFromAssets(context, "shaders/starfield.vert", "shaders/starfield.frag", simplePlane.vertexFormat)
 
-        layer0 = Texture.loadFromAssets(context, "images/stars_layer0.png", Texture.WrapMode.ClampToEdge, Texture.WrapMode.ClampToEdge, Texture.Filtering.LinearMipmapLinear, Texture.Filtering.Linear)
+        layers[0] = Texture.loadFromAssets(context, "images/stars_layer0.png", Texture.WrapMode.Repeat, Texture.WrapMode.Repeat, Texture.Filtering.LinearMipmapLinear, Texture.Filtering.Linear)
+        layers[1] = Texture.loadFromAssets(context, "images/stars_layer1.png", Texture.WrapMode.Repeat, Texture.WrapMode.Repeat, Texture.Filtering.LinearMipmapLinear, Texture.Filtering.Linear)
+        layers[2] = Texture.loadFromAssets(context, "images/stars_layer2.png", Texture.WrapMode.Repeat, Texture.WrapMode.Repeat, Texture.Filtering.LinearMipmapLinear, Texture.Filtering.Linear)
+
+        startTime = SystemClock.uptimeMillis()
     }
 
 
     override fun onSurfaceChanged(p0: GL10?, width: Int, height: Int) {
         GLES20.glViewport(0, 0, width, height)
-        aspect = width.toFloat() / height.toFloat()
+        if(width < height) {
+            aspect = vector2f(width.toFloat() / height.toFloat(), 1.0f)
+        } else {
+            aspect = vector2f(1.0f, height.toFloat() / width.toFloat())
+        }
         Log.debug("onSurfaceChanged(width = $width, height = $height)")
     }
 
     override fun onDrawFrame(p0: GL10?) {
+
+        val deltaTime = 0.0001f * (SystemClock.uptimeMillis() - startTime).toFloat()
+
         GLES20.glClearColor(0.0f, 0.0f, 0.0f, 0.0f)
         GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT)
 
-        simplicityShader.bind()
-        layer0.bind(0)
-        simplicityShader.setSampler("u_Layer0", 0)
-        simplicityShader.setUniformValue("u_Aspect", vector2f(aspect, 1.0f))
+        shader.bind()
+        layers[0].bind(0)
+        layers[1].bind(1)
+        layers[2].bind(2)
+        shader.setSampler("u_Layer0", 0)
+        shader.setSampler("u_Layer1", 1)
+        shader.setSampler("u_Layer2", 2)
+        shader.setUniformValue("u_Time", deltaTime)
+        shader.setUniformValue("u_Aspect", aspect)
         simplePlane.draw()
-        layer0.release(0)
-        simplicityShader.release()
+        layers[0].release(0)
+        layers[1].release(1)
+        layers[2].release(2)
+        shader.release()
     }
 }
