7b3ee2c36957f58db01a3ee8c9c54b3cc5297de4
drdwrd <drwdrd@gmail.com>
2019-01-05 15:53:12 +0100

    alpha version some cleaning

--- Starfield/src/main/assets/shaders/starfield.frag
+++ Starfield/src/main/assets/shaders/starfield.frag
@@ -2,25 +2,28 @@
 precision highp float;
 
 uniform sampler2D u_Starfield;
-uniform vec2 u_Offset;
+uniform sampler2D u_Noise;
 uniform float u_Time;
 
 varying vec2 uv;
-
+varying vec2 center;
 
 const vec3 lumaCoeff = vec3(0.2126, 0.7152, 0.0722);
-const vec2 center = vec2(0.5, 0.5);
 
 void main() {
 
-    vec2 tex = 0.5 + 0.5 * uv + u_Offset;
+    vec2 tex = 0.5 + 0.5 * uv;
+    vec2 c = 0.5 + 0.5 * center;
+
     vec4 starfield = texture2D(u_Starfield, tex);
+
+    vec4 noise = texture2D(u_Noise, 0.5 * uv + vec2(0.1 * u_Time, 0.0));
 
     float luma = min(1.0, dot(lumaCoeff, starfield.rgb));
 
 
-    float d = 1.0 + 0.75 * sin(60.0 * dot(tex - center, tex - center) - 2.0 * u_Time) * smoothstep(0.25, 0.6, luma);
+    float d = 1.0 + 0.75 * sin(60.0 * dot(tex - c, tex - c) - 2.0 * u_Time) * smoothstep(0.25, 0.6, luma);
 
-    gl_FragColor = pow(starfield, vec4(d));
+    gl_FragColor = pow(starfield, vec4(d + 0.5 * (noise.r - 0.5)));
 }
 
--- Starfield/src/main/java/drwdrd/ktdev/starfield/StarfieldRenderer.kt
+++ Starfield/src/main/java/drwdrd/ktdev/starfield/StarfieldRenderer.kt
@@ -28,11 +28,13 @@ class StarfieldRenderer(_context: Context) : GLSurfaceView.Renderer, GLWallpaper
     private lateinit var starFieldShader : ProgramObject
     private lateinit var starFieldTexture : Texture
     private lateinit var starSpritesTexture : Texture
+    private lateinit var noiseTexture : Texture
     private val timer = Timer(0.0002)
     private var lastParticleSpawnTime = 1000.0
     private val sprites : MutableList<StarParticle> = ArrayList()
     private var eye = Eye()
     private var resetGyro  = true
+    private var lastXOffset = 0.0f;
 
     private val maxParticlesCount = 1000        //hard limit just in case...
     private val maxParticleSpawnTime = 0.01
@@ -41,6 +43,9 @@ class StarfieldRenderer(_context: Context) : GLSurfaceView.Renderer, GLWallpaper
     private val lastGravity = vector2f(0.0f, 0.0f)
     private val sensorEventListener = StarfieldSensorEventListener()
     private var gravityOffset = vector2f(0.0f, 0.0f)
+
+    private var randomBackgroundOffset = vector2f(0.0f, 0.0f)
+    private var randomBackgroundRotation = 0.0f
 
     inner class StarfieldSensorEventListener : SensorEventListener {
 
@@ -152,6 +157,8 @@ class StarfieldRenderer(_context: Context) : GLSurfaceView.Renderer, GLWallpaper
     override fun onSurfaceCreated(gl: GL10?, config: EGLConfig?) {
         Log.debug("onSurfaceCreated()")
 
+        RandomGenerator.seed(RandomGenerator.createSeed())
+
         var sensorManager = context.getSystemService(Context.SENSOR_SERVICE) as SensorManager
         var gravitySensor = sensorManager.getDefaultSensor(Sensor.TYPE_GRAVITY)
         sensorManager.registerListener(sensorEventListener, gravitySensor, SensorManager.SENSOR_DELAY_FASTEST)
@@ -171,7 +178,8 @@ class StarfieldRenderer(_context: Context) : GLSurfaceView.Renderer, GLWallpaper
         starFieldShader = ProgramObject.loadFromAssets(context, "shaders/starfield.vert", "shaders/starfield.frag", simplePlane.vertexFormat)
 
         starSpritesTexture = Texture.loadFromAssets(context, "images/starsprites.png", Texture.WrapMode.ClampToEdge, Texture.WrapMode.ClampToEdge, Texture.Filtering.LinearMipmapLinear, Texture.Filtering.Linear)
-        starFieldTexture = Texture.loadFromAssets(context, "images/starfield.png", Texture.WrapMode.ClampToEdge, Texture.WrapMode.ClampToEdge, Texture.Filtering.LinearMipmapLinear, Texture.Filtering.Linear)
+        starFieldTexture = Texture.loadFromAssets(context, "images/starfield.png", Texture.WrapMode.Repeat, Texture.WrapMode.Repeat, Texture.Filtering.LinearMipmapLinear, Texture.Filtering.Linear)
+        noiseTexture = Texture.loadFromAssets(context, "images/noise.png", Texture.WrapMode.Repeat, Texture.WrapMode.Repeat, Texture.Filtering.LinearMipmapLinear, Texture.Filtering.Linear)
 
         eye.setPerspective(50.0f, 0.0f, 100.0f)
 
@@ -195,6 +203,8 @@ class StarfieldRenderer(_context: Context) : GLSurfaceView.Renderer, GLWallpaper
     override fun onSurfaceChanged(p0: GL10?, width: Int, height: Int) {
         GLES20.glViewport(0, 0, width, height)
         resetGyro = true
+        randomBackgroundOffset = RandomGenerator.rand2f(-1.0f, 1.0f)
+        randomBackgroundRotation = RandomGenerator.randf(-M_PI, M_PI)
         eye.setViewport(vector2f(width.toFloat(), height.toFloat()))
         if(width < height) {
             aspect = vector2f(width.toFloat() / height.toFloat(), 1.0f)
@@ -219,19 +229,29 @@ class StarfieldRenderer(_context: Context) : GLSurfaceView.Renderer, GLWallpaper
 
         timer.tick()
 
+
+        var backgroundTextureMatrix = matrix3f()
+        backgroundTextureMatrix.loadIdentity()
+        backgroundTextureMatrix.setRotationPart(randomBackgroundRotation)
+        backgroundTextureMatrix.setTranslationPart(randomBackgroundOffset)
+
+
         //render background
+
+        simplePlane.bind()
+        noiseTexture.bind(2)
 
         starFieldShader.bind()
         starFieldTexture.bind(1)
 
         starFieldShader.setUniformValue("u_Aspect", aspect)
         starFieldShader.setSampler("u_Starfield", 1)
+        starFieldShader.setSampler("u_Noise", 2)
+        starFieldShader.setUniformValue("u_TextureMatrix", backgroundTextureMatrix)
         starFieldShader.setUniformValue("u_Offset", gravityOffset)
         starFieldShader.setUniformValue("u_Time", timer.currentTime.toFloat())
 
-        simplePlane.bind()
         simplePlane.draw()
-        simplePlane.release()
 
         starFieldTexture.release(1)
         starFieldShader.release()
@@ -255,8 +275,7 @@ class StarfieldRenderer(_context: Context) : GLSurfaceView.Renderer, GLWallpaper
         starSpritesTexture.bind(0)
 
         starSpriteShader.setSampler("u_StarSprites", 0)
-
-        simplePlane.bind()
+        starSpriteShader.setSampler("u_Noise", 2)
 
         var it = sprites.iterator()
         while(it.hasNext()) {
@@ -272,8 +291,12 @@ class StarfieldRenderer(_context: Context) : GLSurfaceView.Renderer, GLWallpaper
 
             var fadeIn = smoothstep(0.0f, 1.0f, sprite.age)
 
+            var rotMatrix = matrix3f()
+            rotMatrix.setRotation(0.4f * timer.currentTime.toFloat())
+
 
             starSpriteShader.setUniformValue("u_ModelViewProjectionMatrix", viewProjectionMatrix * sprite.calculateBillboardModelMatrix(dir, normal))
+            starSpriteShader.setUniformValue("u_RotationMatrix", rotMatrix)
             starSpriteShader.setUniformValue("u_uvRoI", vector4f(sprite.uvRoI.left, sprite.uvRoI.top, sprite.uvRoI.width, sprite.uvRoI.height))
             starSpriteShader.setUniformValue("u_FadeIn", fadeIn)
             simplePlane.draw()
@@ -281,14 +304,18 @@ class StarfieldRenderer(_context: Context) : GLSurfaceView.Renderer, GLWallpaper
             sprite.tick(timer.deltaTime.toFloat())
         }
 
-        simplePlane.release()
 
         starSpritesTexture.release(0)
         starSpriteShader.release()
+
+        noiseTexture.release(2)
+        simplePlane.release()
+
     }
 
     override fun onOffsetChanged(xOffset: Float, yOffset: Float, xOffsetStep: Float, yOffsetStep: Float, xPixelOffset: Int, yPixelOffset: Int) {
-
+        gravityOffset.minusAssign(vector2f(0.1f * (xOffset - lastXOffset), 0.0f))
+        lastXOffset = xOffset
     }
 
     override fun onResume() {
