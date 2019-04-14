15625ac157f2dd3f892dc5093f083837b5943306
drwdrd <drwdrd@gmail.com>
2019-04-14 11:20:47 +0200

    ...

--- starfield/src/main/java/drwdrd/ktdev/starfield/StarfieldSettingsActivity.kt
+++ starfield/src/main/java/drwdrd/ktdev/starfield/StarfieldSettingsActivity.kt
@@ -5,14 +5,8 @@ import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentManager
-import androidx.fragment.app.FragmentStatePagerAdapter
-import androidx.viewpager.widget.ViewPager
-import com.google.android.material.tabs.TabLayout
 import com.google.firebase.auth.FirebaseAuth
 import drwdrd.ktdev.engine.Log
-import java.lang.Exception
 
 
 private const val TAG = "drwdrd.ktdev.starfield.StarfieldSettingsActivity"
@@ -20,18 +14,15 @@ private const val TAG = "drwdrd.ktdev.starfield.StarfieldSettingsActivity"
 class StarfieldSettingsActivity : AppCompatActivity() {
 
     private val settingsFragment = SettingsFragment()
-    private val themesFragment = ThemesFragment()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setupActionBar()
-        setContentView(R.layout.settings_activity)
-
-        val viewPager = findViewById<ViewPager>(R.id.settingsViewPager)
-        viewPager.adapter = SettingsPagerAdapter(supportFragmentManager)
-
-        val tabLayout = findViewById<TabLayout>(R.id.settingsTabLayout)
-        tabLayout.setupWithViewPager(viewPager)
+        if(savedInstanceState == null) {
+            val transaction = supportFragmentManager.beginTransaction()
+            transaction.add(android.R.id.content, settingsFragment)
+            transaction.commit()
+        }
 
         val firebaseAuth = FirebaseAuth.getInstance()
         if(firebaseAuth.currentUser == null) {
@@ -81,28 +72,4 @@ class StarfieldSettingsActivity : AppCompatActivity() {
         SettingsProvider.save(applicationContext,"starfield.ini")
         super.onStop()
     }
-
-    inner class SettingsPagerAdapter(fragmentManager: FragmentManager) : FragmentStatePagerAdapter(fragmentManager) {
-
-        override fun getPageTitle(position: Int): CharSequence? {
-            return when(position) {
-                0 -> "Settings"
-                1 -> "Themes"
-                else -> null
-            }
-        }
-
-        override fun getItem(position: Int): Fragment {
-            return when(position) {
-                0 -> settingsFragment
-                1 -> themesFragment
-                else -> throw Exception("Invalid fragment!")
-            }
-        }
-
-        override fun getCount(): Int {
-            return 2
-        }
-    }
-
 }
