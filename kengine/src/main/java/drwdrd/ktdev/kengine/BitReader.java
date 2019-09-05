0567abf6aed0de4c9d8c6a0ae6a6386b3526b719
drwdrd <drwdrd@gmail.com>
2019-09-05 18:46:42 +0200

    cleanup

--- kengine/src/main/java/drwdrd/ktdev/kengine/BitReader.java
+++ kengine/src/main/java/drwdrd/ktdev/kengine/BitReader.java
@@ -2,21 +2,21 @@ package drwdrd.ktdev.kengine;
 
 public interface BitReader {
 
-    public short readShort(byte[] data, int offset);
+    short readShort(byte[] data, int offset);
 
-    public byte readByte(byte[] data, int offset);
+    byte readByte(byte[] data, int offset);
 
-    public int readInt(byte[] data, int offset);
+    int readInt(byte[] data, int offset);
 
-    public long readLong(byte[] data, int offset);
+    long readLong(byte[] data, int offset);
 
-    public float readFloat(byte[] data, int offset);
+    float readFloat(byte[] data, int offset);
 
-    public double readDouble(byte[] data, int offset);
+    double readDouble(byte[] data, int offset);
 
-    public void swapw(byte[] data, int off, int count);
+    void swapw(byte[] data, int off, int count);
 
-    public void swapdw(byte[] data, int off, int count);
+    void swapdw(byte[] data, int off, int count);
 
-    public void swapqw(byte[] data, int off, int count);
+    void swapqw(byte[] data, int off, int count);
 }
